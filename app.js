c89a62d8b8054bd1c1d7bcb6c69144d229bf027e
Zainabsoomro6789 <Zainabsoormo6789@gmail.com>
2023-10-05 03:04:30 +0500

    ready to commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// q1
+// alert("zainab");
+// alert("zainab hassan");
+// alert("zainabsoomoro6789.@gmail.com");
+// alert("0312324237");
+// alert("6324762378");
+// q2
+// alert("You're learning JavaScript!");
+// q3
+// alert("my name is zainab");
+// chapter2
+// q1
+// var myName = "zainab";
+// alert(myName);
+// q2
+// var name;
+// name = "zainab";
+// alert(name);
+// Q 3
+// var teamName = "best buddys";
+// alert(teamName);
+
+// chap 3\
+// Q1
+// var caseQty;
+// Q2
+// var sum = 6;
+// var result = sum + 2;
+// alert(result);
+// q3
+// var merchTotal 
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal);
+// chap 4
+// q1
+// var product_cost = 3.45;
+// q2
+// var NameOfBand;
+// q3
+// var firstName = "zainab";
+// var lastName = "hassan";
+// alert(firstName + lastName);
+// chapter 5
+// var num = 20 % 6;
+// alert(num);
+// q2
+// var number = 100 % 2;
+// alert(number);
+// q3
+// var number = 50-25;
+// alert(number);
